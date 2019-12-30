825d7f5a8205d0b746097482d92bd7299c2a2121
RayZen <jake2011ren@gmail.com>
2019-12-30 23:30:55 +0800

    Add response query by using js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const menuIcon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menue");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+
+// add pop up search box
+searchIcon.addEventListener('click', () => {
+  if(searchBox.style.top === '72px'){
+    searchBox.style.top = '24px';
+    searchBox.style.pointerEvents = 'none';
+  } else {
+    searchBox.style.top = '72px';
+    searchBox.style.pointerEvents = 'auto';
+  }
+})
+
+// show slideoutMenu
+menuIcon.addEventListener('click', () => {
+  if(slideoutMenu.style.opacity === '1'){
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+  } else {
+    slideoutMenu.style.opacity = '1';
+    slideoutMenu.style.pointerEvents = 'auto';
+  }
+})
